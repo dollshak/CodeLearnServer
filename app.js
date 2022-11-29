988675e47e91a82299c7440a04f7270495369557
dollshak <dollshak@post.bgu.ac.il>
2022-11-29 12:42:40 +0200

    added socket

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const sessionRoutes = require('./routs/sessionRouts')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const http = require("http")
+const {Server} = require("socket.io")
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,8 +23,11 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const uri = 'mongodb+srv://Shaked:123@codelearn.uqobdhi.mongodb.net/?retryWrites=true&w=majority'
-app.listen(port, () => console.log('server started'));
+// app.listen(port, () => console.log('server started'));
 
+server.listen(port, () => {
+    console.log("listening")
+})
 async function connect(){
     try {
         await mongoose.connect(uri)
@@ -31,6 +43,27 @@ connect()
 app.use('/users', usersRouts);
 app.use('/codeBlock', codeBlockRoutes)
 app.use('/session', sessionRoutes)
-// app.use(bodyParser.json())
-// app.use(bodyParser .urlencoded({ extended: true }))
+
+io.on("connection", (socket) => {
+    console.log(`user connected: ${socket.id}`);
+
+    socket.on("join_session", (data) => {
+        socket.join(data);
+    });
+    socket.on("update_code", (data) => {
+        console.log("data from client", data);
+        socket.to(data.sessionUuid).emit("receive_updated_code", data)
+    })
+
+
+    // socket.on("send_message", (data) => {
+    //     socket.broadcast.emit("receive_message", data)
+    // });
+
+    // socket.on("join_room", (data) => {
+    //     socket.join(data);
+    // })
+});
+
+
 
