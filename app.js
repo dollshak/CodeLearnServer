f7bb63d6f381760ff2e61ee4fcde5a6c68dd0ea4
dollshak <dollshak@post.bgu.ac.il>
2022-12-01 16:48:37 +0200

    added catch

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ app.use(cors());
 
 const port = process.env.PORT || 4000;
 
-server.listen(port, () => {
-    console.log("server started on port", port)
-})
+try{
+    server.listen(port, () => {
+        console.log("server started on port", port);
+    })
+}
+catch(err){
+    console.log("error");
+}
 
 const uri = process.env.DB_CONNECTION;
 
